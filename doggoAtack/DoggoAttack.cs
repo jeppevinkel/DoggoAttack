04e2b0eca0116d98ffd10bda26b071ab6210cac7
jeppevinkel <jeppevinkel@gmail.com>
2019-03-21 15:15:06 +0100

    Add project files.

---
+++ doggoAtack/DoggoAttack.cs
@@ -0,0 +1,43 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace doggoAttack
+{
+    [PluginDetails(
+        author = "Jopo",
+        name = "DoggoAttack",
+        description = "Doggo attack event",
+        id = "jopo.doggoattack.plugin",
+        version = "2.4",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class DoggoAttack : Plugin
+    {
+        static IConfigFile Config => ConfigManager.Manager.Config;
+
+        public bool isActive = false;
+
+        public override void OnDisable()
+        {
+            this.Info(this.Details.name + " was disabled ):");
+        }
+
+        public override void OnEnable()
+        {
+            this.Info(this.Details.name + " has loaded :)");
+        }
+        
+        public override void Register()
+        {
+            // Register Events
+            this.AddEventHandlers(new EventHandler(this));
+
+            // Register Commands
+            this.AddCommand("da_start", new ConfigCmd(this));
+        }
+    }
+}
---
+++ doggoAtack/EventHandler.cs
@@ -0,0 +1,42 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doggoAttack
+{
+    class EventHandler : IEventHandlerWaitingForPlayers, IEventHandlerPlayerDie
+    {
+        static IConfigFile Config => ConfigManager.Manager.Config;
+        private static readonly System.Random getrandom = new System.Random();
+        private DoggoAttack plugin;
+
+        public EventHandler(DoggoAttack plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+        {
+
+        }
+
+        public void OnPlayerDie(PlayerDeathEvent ev)
+        {
+            if (!plugin.isActive)
+            {
+                return;
+            }
+            if ((byte)ev.Player.TeamRole.Role == 1 && ((byte)ev.Killer.TeamRole.Role == 16 | (byte)ev.Killer.TeamRole.Role == 17))
+            {
+                ev.SpawnRagdoll = false;
+                ev.Player.ChangeRole(Smod2.API.Role.SCP_939_89, true, false, false, true);
+            }
+        }
+    }
+}
